0d1f7df3a47015e104fa6ef7b8481ccc1bd11d65
Alex Student <alex.smith.tr@gmail.com>
2022-05-28 23:01:39 +0300

    calculator

---
+++ buttons.js
@@ -0,0 +1,59 @@
+let num1 = () =>  {
+    document.getElementById("output").value += "1";
+}
+let num2 = () =>  {
+    document.getElementById("output").value += "2";
+}
+
+let num3 = () =>  {
+    document.getElementById("output").value += "3";
+}
+
+let num4 = () =>  {
+    document.getElementById("output").value += "4";
+}
+
+let num5 = () =>  {
+    document.getElementById("output").value += "5";
+}
+
+let num6 = () =>  {
+    document.getElementById("output").value += "6";
+}
+
+let num7 = () =>  {
+    document.getElementById("output").value += "7";
+}
+
+let num8 = () =>  {
+    document.getElementById("output").value += "8";
+}
+
+let num9 = () =>  {
+    document.getElementById("output").value += "9";
+}
+
+let num0 = () =>  {
+    document.getElementById("output").value += "0";
+}
+
+let dot = () =>  {
+    document.getElementById("output").value += ".";
+}
+
+let plus = () =>  {
+    document.getElementById("output").value += " + ";
+}
+
+let minus = () =>  {
+    document.getElementById("output").value += " - ";
+}
+
+let mult = () =>  {
+    document.getElementById("output").value += " * ";
+}
+
+let divide = () =>  {
+    document.getElementById("output").value += " / ";
+}
+
---
+++ equal.js
@@ -0,0 +1,5 @@
+let textAreaValue = document.querySelector("#output");
+
+let equal = () => {
+   document.getElementById("output").value = eval(textAreaValue.value) 
+}
